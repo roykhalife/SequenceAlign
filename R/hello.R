970a5852e46daf35ecb2b7ac8d315c7bb559a885
roykhalife <roykhalife97@gmail.com>
2020-04-18 11:57:03 -0400

    new data and updated code

--- R/hello.R
+++ R/hello.R
@@ -1,18 +1,14 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
+fasta1 <- "data/sequence1.txt"
+fasta2 <- "data/sequence2.txt"
 
-hello <- function() {
-  print("Hello, world!")
+CountAA <- function(AA)
+{
+
+  addedlines <- nchar(readLines(fasta1))[-1]
+  sum(addedlines)
+  AA <- sum(addedlines)
+  print(paste("The are", AA, "Amino Acids in this protein."))
+
 }
+
+CountAA(fasta1)
